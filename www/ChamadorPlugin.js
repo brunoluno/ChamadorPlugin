1bb54b93d29bc73554d1c0338c8873d93e146532
Jefferson Bruno <brunoluno10@gmail.com>
2018-02-17 22:08:49 -0300

    Adjustments

--- www/ChamadorPlugin.js
+++ www/ChamadorPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (success, error) {
+exports.getMedias = function (success, error) {
     exec(success, error, 'ChamadorPlugin', 'getMedias', []);
 };
